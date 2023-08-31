91b08dc539c73f30cc896019a02b09df7cd00400
vinisha05-c <vinishadec02>
2023-08-31 14:11:51 +0530

    python

---
+++ Attribute/six.py
@@ -0,0 +1,15 @@
+class account:
+    min-Bal=500
+    a1=account()
+    a2=account()
+    a3=account()
+    print(a1)
+    print(id(a1))
+    print(a1._dict_)
+    print(a2)
+    print(id(a2))
+    prnt(a2._dict_)
+    print(a3)
+    print(id(a3))
+    print(a3._dict_)
+
