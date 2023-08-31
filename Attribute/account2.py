c0dd9225f58fb4f39ced6e328a2bdd75aaefcdf5
vinisha05-c <vinishadec02>
2023-08-31 15:48:12 +0530

    account

---
+++ Attribute/account2.py
@@ -0,0 +1,13 @@
+class account:
+  def open_account(sef):
+    print("account successfully")
+    def deposit_amount (sef):
+        print("amount deposited")
+        a1=account()
+        print(a1)
+        a1.open_account()
+        a1.deposit_amount()
+        print(id(a1))
+        print(a1._dict_)
+
+    
