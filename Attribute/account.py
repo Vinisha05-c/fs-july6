fe28fff9e745969e5ff2829f3f033fcc3988fc7b
vinisha05-c <vinishadec02>
2023-08-31 16:02:40 +0530

    account

---
+++ Attribute/account.py
@@ -0,0 +1,12 @@
+class Account:
+    
+    def __init__(self):
+       print("constructor method execute automatically")
+    def open_account(delf):
+        print("account successfully")
+        def deposit_amount(self):
+            print("amount deposited")
+            account()
+            account()
+            account()
+            account()
